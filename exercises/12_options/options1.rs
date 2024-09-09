f5e382ecfb11bcc26c6f9dbeadbcbd1cdc2c1d68
Teeco123 <kacper.gajko1@gmail.com>
2024-09-09 11:48:16 +0200

    Done options section

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -1,9 +1,18 @@
+
 // This function returns how much icecream there is left in the fridge.
 // If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
 // someone eats it all, so no icecream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day < 22{
+        Some(5)
+    } else if hour_of_day > 23{
+        None
+    } else{
+        Some(0)
+    }
+
 }
 
 fn main() {
@@ -18,7 +27,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
+        let icecreams = maybe_icecream(12).unwrap();
 
         assert_eq!(icecreams, 5); // Don't change this line.
     }
--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -10,8 +10,13 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if-let statement whose value is `Some`.
-        word = optional_target {
-            assert_eq!(word, target);
+
+        //word = optional_target {
+        //    assert_eq!(word, target);
+        //}
+
+        if let Some(word) = optional_target {
+            assert_eq!(word,target)
         }
     }
 
@@ -29,8 +34,14 @@ mod tests {
         // TODO: Make this a while-let statement. Remember that `Vec::pop()`
         // adds another layer of `Option`. You can do nested pattern matching
         // in if-let and while-let statements.
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
+
+        //integer = optional_integers.pop() {
+        //    assert_eq!(integer, cursor);
+        //    cursor -= 1;
+        //}
+        
+        while let Some(Some(int)) = optional_integers.pop() {
+            assert_eq!(int, cursor);
             cursor -= 1;
         }
 
